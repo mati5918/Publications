549e03464a3a9dff7dd012634355c73f6bc67039
pbudzinski <pbudzinski@hotmail.com>
2016-12-12 18:48:28 +0100

    Add title field to publication entity

---
+++ src/Publications/Data/Migrations/20161212174529_AddTitleFieldToPublicationEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Publications.Data.Migrations
+{
+    public partial class AddTitleFieldToPublicationEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Publications",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Publications");
+        }
+    }
+}
